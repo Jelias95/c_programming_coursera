447f6796d9aae07b9ce0d1b3ba5aae1d599d9c90
Coursera Learner <nobody@nowhere.nul>
2019-06-17 01:08:52 +0000

    fixed stars misspelling

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -1,8 +1,7 @@
 
 int printTriangle(int size) {
-  //start with starCount being 0
   int starCount = 0;
-  //count from 0 (inclusive) to size (exclusive), for each number i that you count
+  
   for (int i = 0; i < size; i++) {
     for (int j = 0; j <= i; j++) {
       printf("*");
@@ -10,38 +9,21 @@ int printTriangle(int size) {
     }
     printf("\n");
   }
-     //count from 0 (inclusive) to i (inclusive), for each number j that you count
+  
   return starCount;
-        //print a "*"
-
-        //increment starCount
-
-     //when you finish counting on j, 
-
-     //print a newline ("\n")
-
-  //when you finish counting on i, 
-
-  //your answer is starCount
-
 }
 
 
 int main(void) {
   int numStars;
+
   printf("Here is a triangle with height 4\n");
   numStars = printTriangle(4);
   printf("That triangle had %d total stars\n", numStars);
+
   printf("Here is a triangle with height 7\n");
   numStars = printTriangle(7);
-  printf("That triangle had %d total starts\n", numStars);
-  //now print "Here is a triangle with height 7\n"
-
-  //then call printTriangle, passing in 7, and assign the result to numStars
-
-  //finally, print "That triangle had %d total stars\n", such that the %d 
-  //prints the value of numStars
-
+  printf("That triangle had %d total stars\n", numStars);
 
   return 0;
 }
